fa5c52c5e361063e017029d55bc8aa37e675eb6f
Yun-D <yund_272@naver.com>
2021-01-10 02:18:11 +0900

    ✨ 로그인 기능 추가

--- budD_android/app/src/main/java/com/example/budd/LoginActivity.java
+++ budD_android/app/src/main/java/com/example/budd/LoginActivity.java
@@ -3,13 +3,21 @@ package com.example.budd;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    ImageButton login;
+    EditText editUserid, editUserpw;
+    Button loginBtn;
+
+    static public String userID = "test111";
+    static public String userPWD = "test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +28,18 @@ public class LoginActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        login = (ImageButton) findViewById(R.id.loginBtn);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        editUserid = findViewById(R.id.editUserid);
+        editUserpw = findViewById(R.id.editUserpw);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                if (editUserid.getText().toString().equals(userID) && editUserpw.getText().toString().equals(userPWD)) {
+                    Intent intent = new Intent(getApplicationContext(), NewPage_selectInter.class);
+                    startActivity(intent); //액티비티 띄우기
+                } else
+                    Toast.makeText(getApplicationContext(), "아이디나 패스워드가 틀립니다.", Toast.LENGTH_SHORT).show();
             }
         });
     }
